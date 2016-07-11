c1bfbf1397cba932c006bf44142f1ab0cc7f0113
Noah Lipsyc <noah@Noahs-MacBook-Pro.local>
2016-07-11 15:55:45 -0400

    All done, pushes to multiple spreadsheets, one a full log of changes and changless scrapes, the other just changes

---
+++ websites.py
@@ -0,0 +1,60 @@
+# The pages we're going to scrape
+pages = {
+    '1': {
+        'title': 'photobucketImport',
+        'url': 'http://support.photobucket.com/hc/en-us/articles/200724324-Linking-and-Embedding-Images'
+        },
+    '2': {
+        'title': '2Checkout',
+        'url': 'http://help.2checkout.com/articles/FAQ/How-Does-The-Return-Process-Work/?q=Given+links+back+to+my+website&l=en_US&fs=Search&pn=1'
+        },
+    '3': {
+        'title': 'authorizeNet',
+        'url': 'http://developer.authorize.net/support/faqs/'
+        },
+    '4': {
+        'title': 'bluepay',
+        'url': 'https://www.bluepay.com/support/faqs/where-do-i-find-account-id-and-secret-key-my-bluepay-gateway/'
+        },
+    '5': {
+        'title': 'skrill',
+        'url': 'http://help.skrill.com/en/Article/business/skrill-basics/where-do-i-find-my-secret-word-so-i-can-link-skrill-with-my-shopping-cart'
+        },
+    '6': {
+        'title': 'stripe',
+        'url': 'https://support.stripe.com/questions/where-do-i-find-my-api-keys'
+        },
+    '7': {
+        'title': 'twitter',
+        'url': 'https://support.twitter.com/articles/20170071'
+        },
+    '8': {
+        'title': 'zeemapsCreate',
+        'url': 'https://content.zeemaps.com/mapping/create-a-map/'
+        },
+    '9': {
+        'title': 'zeemapsPublish',
+        'url': 'https://content.zeemaps.com/mapping/publishing-and-sharing-your-map/'
+        },
+    '10': {
+        'title': 'photobucketSharing',
+        'url': 'http://support.photobucket.com/hc/en-us/articles/200931740-Sharing-an-Album'
+        },
+    # '11': {
+    #     'title': 'shopify',
+    #     'url': 'https://help.shopify.com/manual/sell-online/buy-button/create-buy-button'
+    #     },
+    '12': {
+        'title': 'ecwid',
+        'url': 'https://support.ecwid.com/hc/en-us/articles/207359989-Product-Browser'
+        },
+    '13': {
+        'title': 'googleAnalytics',
+        'url': 'https://support.google.com/analytics/answer/1032385?hl=en'
+        },
+    '14': {
+        'title': 'googleFonts',
+        'url': 'https://developers.google.com/fonts/docs/getting_started'
+        },
+    }
+xpath = ''
